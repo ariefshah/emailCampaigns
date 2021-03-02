d81a6b85bc382873f8daa4cec2b0506ff8a63e8f
ariefshah <arief.shah@gmail.com>
2021-03-03 10:51:34 +1100

    Update upload-to-s3.bat

--- upload-to-s3.bat
+++ upload-to-s3.bat
@@ -1,9 +1,9 @@
 #!/bin/bash
-aws s3 cp s3://www.standwithkashmir.org.au/facts/ ./archive --recursive
+aws s3 cp s3://www.standwithkashmir.org.au/fact-test/ ./archive --recursive
 # Empty the S3 bucket
-aws s3 rm s3://www.standwithkashmir.org.au/facts/ --recursive
+aws s3 rm s3://www.standwithkashmir.org.au/fact-test/ --recursive
 
 # Upload our project files to the S3 bucket
-aws s3 cp dist/ s3://www.standwithkashmir.org.au/facts/ --recursive
+aws s3 cp dist/ s3://www.standwithkashmir.org.au/fact-test/ --recursive
 
 aws cloudfront create-invalidation --distribution-id E2E8RFVSJXH6JE --paths "/*"
